d43198e916b24bee5a80597c0c95cd90e485dd12
Chris Grimmett <chris@grimtech.net>
2023-02-07 13:45:54 -0800

    redact google api key

--- src/data/spreadsheet.js
+++ src/data/spreadsheet.js
@@ -1,5 +1,9 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
-const spreadsheetUrl = 'https://sheets.googleapis.com/v4/spreadsheets/1ChEgFmSQ7LnyHkHGX0WDXE7v1vuWLJ2TkveZf0FtISI/values/Sheet1?key=AIzaSyB-16Q1shTe4jbaOHbjVLqyjX7k1RFwUgM'
+
+
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
+if (typeof GOOGLE_API_KEY === 'undefined') throw new Error('GOOGLE_API_KEY is undefined in env!')
+const spreadsheetUrl = `https://sheets.googleapis.com/v4/spreadsheets/1ChEgFmSQ7LnyHkHGX0WDXE7v1vuWLJ2TkveZf0FtISI/values/Sheet1?key=${GOOGLE_API_KEY}`
 
 async function fetchJson(url) {
   console.log(`  >> getching ${url}`)
